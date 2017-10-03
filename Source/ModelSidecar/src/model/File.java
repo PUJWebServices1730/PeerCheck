93c9ced743ea1814ea371906a5c7355ea73f9c90
davl3232 <davl3232@gmail.com>
2017-10-02 20:44:56 -0500

    Created class File.

---
+++ Source/ModelSidecar/src/model/File.java
@@ -0,0 +1,16 @@
+
+package model;
+
+/**
+ * Representa un archivo del sistema.
+ * @author davl3232
+ */
+public class File {
+	String content;
+	String path;
+
+	public File(String content, String path) {
+		this.content = content;
+		this.path = path;
+	}
+}
