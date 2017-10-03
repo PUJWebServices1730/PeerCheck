b80c26b8785ef946fe2a28659c74f9364ce321aa
davl3232 <davl3232@gmail.com>
2017-10-02 20:49:02 -0500

    Event, article & file classes

---
+++ Source/ModelSidecar/src/model/File.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author davlad
+ */
+public class File {
+	private String content;
+	private String path;
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+}
