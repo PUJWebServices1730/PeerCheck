00e728205a44a86e47574dc336d1bb8012b8cec2
Kaykos <bobadillasebas@gmail.com>
2017-10-03 21:10:50 -0500

    Created entities from database

---
+++ Source/ModelSidecar/src/model/Topics.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author sebas
+ */
+@Entity
+@Table(name = "TOPICS")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Topics.findAll", query = "SELECT t FROM Topics t")
+    , @NamedQuery(name = "Topics.findById", query = "SELECT t FROM Topics t WHERE t.id = :id")
+    , @NamedQuery(name = "Topics.findByName", query = "SELECT t FROM Topics t WHERE t.name = :name")})
+public class Topics implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "NAME")
+    private String name;
+    @ManyToMany(mappedBy = "topicsList")
+    private List<Events> eventsList;
+
+    public Topics() {
+    }
+
+    public Topics(Integer id) {
+        this.id = id;
+    }
+
+    public Topics(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @XmlTransient
+    public List<Events> getEventsList() {
+        return eventsList;
+    }
+
+    public void setEventsList(List<Events> eventsList) {
+        this.eventsList = eventsList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Topics)) {
+            return false;
+        }
+        Topics other = (Topics) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "model.Topics[ id=" + id + " ]";
+    }
+    
+}
---
+++ Source/ModelSidecar/src/model/Users.java
@@ -0,0 +1,202 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author sebas
+ */
+@Entity
+@Table(name = "USERS")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Users.findAll", query = "SELECT u FROM Users u")
+    , @NamedQuery(name = "Users.findById", query = "SELECT u FROM Users u WHERE u.id = :id")
+    , @NamedQuery(name = "Users.findByBirthdate", query = "SELECT u FROM Users u WHERE u.birthdate = :birthdate")
+    , @NamedQuery(name = "Users.findByEmail", query = "SELECT u FROM Users u WHERE u.email = :email")
+    , @NamedQuery(name = "Users.findByName", query = "SELECT u FROM Users u WHERE u.name = :name")
+    , @NamedQuery(name = "Users.findByPassword", query = "SELECT u FROM Users u WHERE u.password = :password")
+    , @NamedQuery(name = "Users.findByRole", query = "SELECT u FROM Users u WHERE u.role = :role")})
+public class Users implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "BIRTHDATE")
+    @Temporal(TemporalType.DATE)
+    private Date birthdate;
+    @Basic(optional = false)
+    @Column(name = "EMAIL")
+    private String email;
+    @Basic(optional = false)
+    @Column(name = "NAME")
+    private String name;
+    @Basic(optional = false)
+    @Column(name = "PASSWORD")
+    private String password;
+    @Basic(optional = false)
+    @Column(name = "ROLE")
+    private String role;
+    @JoinTable(name = "USERS_ARTICLES", joinColumns = {
+        @JoinColumn(name = "USER_ID", referencedColumnName = "ID")}, inverseJoinColumns = {
+        @JoinColumn(name = "ARTICLE_ID", referencedColumnName = "ID")})
+    @ManyToMany
+    private List<Articles> articlesList;
+    @OneToMany(mappedBy = "reviewerId")
+    private List<Reviews> reviewsList;
+    @OneToMany(mappedBy = "editorId")
+    private List<Events> eventsList;
+    @OneToMany(mappedBy = "mainAuthorId")
+    private List<Articles> articlesList1;
+
+    public Users() {
+    }
+
+    public Users(Integer id) {
+        this.id = id;
+    }
+
+    public Users(Integer id, Date birthdate, String email, String name, String password, String role) {
+        this.id = id;
+        this.birthdate = birthdate;
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @XmlTransient
+    public List<Articles> getArticlesList() {
+        return articlesList;
+    }
+
+    public void setArticlesList(List<Articles> articlesList) {
+        this.articlesList = articlesList;
+    }
+
+    @XmlTransient
+    public List<Reviews> getReviewsList() {
+        return reviewsList;
+    }
+
+    public void setReviewsList(List<Reviews> reviewsList) {
+        this.reviewsList = reviewsList;
+    }
+
+    @XmlTransient
+    public List<Events> getEventsList() {
+        return eventsList;
+    }
+
+    public void setEventsList(List<Events> eventsList) {
+        this.eventsList = eventsList;
+    }
+
+    @XmlTransient
+    public List<Articles> getArticlesList1() {
+        return articlesList1;
+    }
+
+    public void setArticlesList1(List<Articles> articlesList1) {
+        this.articlesList1 = articlesList1;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Users)) {
+            return false;
+        }
+        Users other = (Users) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "model.Users[ id=" + id + " ]";
+    }
+    
+}
