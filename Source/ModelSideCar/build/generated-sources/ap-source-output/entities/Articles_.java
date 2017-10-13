08b3df5b476082b1834b6d99bf39b478d37c768f
Kaykos <bobadillasebas@gmail.com>
2017-10-13 16:08:48 -0500

    Updated events service

--- Source/ModelSideCar/build/generated-sources/ap-source-output/entities/Articles_.java
+++ Source/ModelSideCar/build/generated-sources/ap-source-output/entities/Articles_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-10-12T19:40:13")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-10-13T16:07:21")
 @StaticMetamodel(Articles.class)
 public class Articles_ { 
 
--- Source/ModelSideCar/build/generated-sources/ap-source-output/entities/Files_.java
+++ Source/ModelSideCar/build/generated-sources/ap-source-output/entities/Files_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-10-12T19:40:13")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-10-13T16:07:21")
 @StaticMetamodel(Files.class)
 public class Files_ { 
 
