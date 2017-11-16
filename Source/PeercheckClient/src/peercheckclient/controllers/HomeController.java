b534c92b187dd484df0b1dbe89ecc7037f59f309
Kaykos <bobadillasebas@gmail.com>
2017-11-16 00:59:28 -0500

    Fixed table updates

--- Source/PeercheckClient/src/peercheckclient/controllers/HomeController.java
+++ Source/PeercheckClient/src/peercheckclient/controllers/HomeController.java
@@ -27,11 +27,17 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TitledPane;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import peercheckclient.PeercheckClient;
@@ -45,97 +51,156 @@ import session.UserSession;
 public class HomeController implements Initializable {
 
     @FXML
-    private Label articleFoundAutor;
+    private Label currentUserNameLabel, currentUserMailLabel, currentUserRoleLabel;
+
+    // ------------- Search article components -------------
+    @FXML
+    private TitledPane paneSearchArticle;
 
     @FXML
-    private Label articleFoundKeywords;
+    private JFXTextField searchArticleQueryTextField;
 
     @FXML
-    private Label labelCorreo;
+    private JFXComboBox searchArticleFilterCombo;
 
     @FXML
-    private Label articleFoundID;
+    private TableView<Articles> searchArticlesTable;
 
     @FXML
-    private Label articleFoundName;
+    private TableColumn searchArticleTableTitleColumn, searchArticleTableCategoryColumn, searchArticleTableKeyWordsColumn;
 
     @FXML
-    private Label labelRol;
+    private Label searchArticleTitleLabel, searchArticleAuthorLabel, searchArticleKeywordsLabel, searchArticleAbstractLabel;
 
+    // ------------- Change role components -------------
     @FXML
-    private Label articleFoundCetegory;
+    private TitledPane paneChangeRole;
 
-    @FXML
-    private JFXTextField searchArticle;
-
-    @FXML
-    private Label labelNombre;
-    
-    @FXML
-    private TableView<Articles> articleFoundTable;
-    
     @FXML
     private TableView<Users> changeRoleUsersTable;
-    
-    @FXML
-    private TableColumn idColumn;
-    
-    @FXML
-    private TableColumn nameColumn;
 
     @FXML
-    private JFXComboBox<ArticleCriteria> comboArticleFilter;
+    private TableColumn changeRoleTableIdColumn, changeRoleTableNameColumn, changeRoleTableRoleColumn;
+
+    @FXML
+    private Label changeRoleIdLabel, changeRoleNameLabel, changeRoleRoleLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resources) {
+
+        ObservableList<ArticleCriteria> articleFilter = FXCollections.observableArrayList(
+                ArticleCriteria.NOMBRE,
+                ArticleCriteria.CATEGORIA
+        );
+
+        searchArticleFilterCombo.setItems(articleFilter);
+        searchArticleFilterCombo.setValue(ArticleCriteria.NOMBRE);
+
+        initComponents();
+    }
+
+    public void initTableColumns() {
+        //Search articles table columns
+        searchArticleTableTitleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
+        searchArticleTableCategoryColumn.setCellValueFactory(new PropertyValueFactory<>("category"));
+        searchArticleTableKeyWordsColumn.setCellValueFactory(new PropertyValueFactory<>("keywords"));
+
+        //Change role table columns
+        changeRoleTableIdColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        changeRoleTableNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        changeRoleTableRoleColumn.setCellValueFactory(new PropertyValueFactory<>("role"));
+    }
+
+    public void setUpListeners() {
+        //Actualizar lables con el artículo seleccionado de la tabla
+        searchArticlesTable.setOnMouseClicked((MouseEvent event) -> {
+            if (event.getButton().equals(MouseButton.PRIMARY)) {
+                if (searchArticlesTable.getSelectionModel().getSelectedItem() != null) {
+                    updateSelectedArticleLabels(searchArticlesTable.getSelectionModel().getSelectedItem());
+                }
+            }
+        });
+
+        //Actualizar lista de usuarios al expandir el panel
+        paneChangeRole.expandedProperty().addListener((obs, wasExpanded, isNowExpanded) -> {
+            if (isNowExpanded) {
+                fillTable(changeRoleUsersTable, PeercheckSOAPController.getAllUsers());
+            }
+        });
+
+        //Actualizar lables con el usuario seleccionado de la tabla
+        changeRoleUsersTable.setOnMouseClicked((MouseEvent event) -> {
+            if (event.getButton().equals(MouseButton.PRIMARY)) {
+                if (changeRoleUsersTable.getSelectionModel().getSelectedItem() != null) {
+                    updateSelectedUserLabels(changeRoleUsersTable.getSelectionModel().getSelectedItem());
+                }
+            }
+        });
+
+    }
+
+    //Actualizar labels de información del artículo seleciconado en el panel de Search Article
+    public void updateSelectedArticleLabels(Articles article) {
+        searchArticleTitleLabel.setText(article.getTitle());
+        searchArticleAuthorLabel.setText(article.getMainAuthorId().getName());
+        searchArticleKeywordsLabel.setText(article.getKeywords());
+        searchArticleAbstractLabel.setText(article.getAbstract1());
+    }
+
+    //Actualizar labels de información del user seleciconado en el panel de Change Role
+    public void updateSelectedUserLabels(Users user) {
+        changeRoleIdLabel.setText(user.getId().toString());
+        changeRoleNameLabel.setText(user.getName());
+        changeRoleRoleLabel.setText(user.getRole());
+    }
+
+    public void setCurrentUserInfo() {
+        currentUserNameLabel.setText(UserSession.user.getName());
+        currentUserMailLabel.setText(UserSession.user.getEmail());
+        currentUserRoleLabel.setText(UserSession.user.getRole());
+    }
+
+    public void initComponents() {
+        setCurrentUserInfo();
+        initTableColumns();
+        setUpListeners();
+    }
+
+    public void fillTable(TableView table, List data) {
+        clearTable(table);
+        table.getItems().addAll(data);
+    }
+
+    public void clearTable(TableView table) {
+        table.getItems().clear();
+    }
 
     @FXML
     public void logout(ActionEvent event) {
         openLogin(event);
         UserSession.user = null;
     }
-    
+
     @FXML
     void findArticle(ActionEvent event) {
-        String query = searchArticle.getText();
-        ArticleCriteria criteria = comboArticleFilter.getValue();
-        System.out.println(criteria + " - " + query);
+        String query = searchArticleQueryTextField.getText();
+        ArticleCriteria criteria = (ArticleCriteria) searchArticleFilterCombo.getValue();
         List<Articles> articles = PeercheckSOAPController.findArticleBy(criteria, query);
-        if(articles.size() > 0) {
-            System.out.println(articles.size());
-            articleFoundTable.getItems().setAll(articles);
-            articleFoundAutor.setText(articles.get(0).getMainAuthorId().getName());
-            articleFoundCetegory.setText(articles.get(0).getCategory());
-            articleFoundID.setText(articles.get(0).getId() + "");
-            articleFoundKeywords.setText(articles.get(0).getKeywords());
-            articleFoundName.setText(articles.get(0).getTitle());
+
+        if (articles.isEmpty()) {
+            clearTable(searchArticlesTable);
+            displayAlertDialog("No se encontraron artículos");
         } else {
-            articleFoundID.setText("No encontrado");
+            fillTable(searchArticlesTable, articles);
         }
     }
-    
+
     @FXML
     void getAllUsers(ActionEvent event) {
-        List<Users> users = PeercheckSOAPController.getAllUsers();
-        System.out.println(users.size());
-        
-        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
-        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));        
-//changeRoleUsersTable.getColumns().addAll(idColumn, nameColumn);
-        changeRoleUsersTable.getItems().addAll(users);
+        //List<Users> users = PeercheckSOAPController.getAllUsers();
+        //changeRoleUsersTable.getItems().addAll(users);
     }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle resources) {
-        labelCorreo.setText(UserSession.user.getEmail());
-        labelRol.setText(UserSession.user.getRole());
-        labelNombre.setText(UserSession.user.getName());
-        
-        ObservableList<ArticleCriteria> articleFilter = FXCollections.observableArrayList (
-            ArticleCriteria.NOMBRE,
-            ArticleCriteria.CATEGORIA
-        );
-        comboArticleFilter.setItems(articleFilter);
-        comboArticleFilter.setValue(ArticleCriteria.NOMBRE);
-    }
-    
+
     @FXML
     void openLogin(ActionEvent event) {
         try {
@@ -146,9 +211,15 @@ public class HomeController implements Initializable {
             stage.setTitle("Peercheck");
             stage.initModality(Modality.APPLICATION_MODAL);
             stage.show();
-            ((Stage)((Node)event.getSource()).getScene().getWindow()).close();
+            ((Stage) ((Node) event.getSource()).getScene().getWindow()).close();
         } catch (IOException ex) {
             Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    private void displayAlertDialog(String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION, message, ButtonType.OK);
+        alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+        alert.show();
     }
 }
